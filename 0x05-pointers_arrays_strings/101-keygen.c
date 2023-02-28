d58aa6f88c20d448304f8e50bfc2c3d175b6ca5f
CatiaPortimao <catiaportimao@gmail.com>
2023-02-28 12:32:11 -0800

    task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,51 +1,23 @@
-#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 /**
- * main - Generates random valid passwords for the program 101-crackme.
+ * main - entry point generates a random password
  *
- * description: finds checksum value from objdump
- *
- * Return: 0
+ * Return: the generated password
  */
 int main(void)
 {
-	int counter, sum, random;
+char c;
+int x;
 
-	sum = 0;
-	counter = 0;
-	random = 0;
-
-	char password[80];
-
-	srand(time(NULL));
-
-	while (sum < 2772)
-	{
-
-		if (2772 - sum < 48)
-	{
-		sum -= password[--counter];
-	}
-		else if (2772 - sum <= 126)
-		{
-			random = 2772 - sum;
-		}
-		else
-		{
-			random = rand() % (126 - 48) + 48;
-		}
-
-		if (random)
-		{
-			password[counter++] = random;
-			sum += random;
-		}
-		random = 0;
-	}
-
-	password[counter] = '\0';
-	printf("%s\n", password);
-
-	return (0);
+srand(time(0));
+while (x <= 2645)
+{
+c = rand() % 128;
+x += c;
+putchar(c);
+}
+putchar(2772 - x);
+return (0);
 }
